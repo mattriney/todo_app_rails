a09ca6176815faf99a47f306248adb2d42dfc9ad
mattriney <61891660+mattriney@users.noreply.github.com>
2022-03-21 18:53:56 -0500

    add helper for is user logged in and current user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        @current_user ||= USer.find_by_id(session[:user_id]) if !!session[user_id]
+        
+    end
 end
